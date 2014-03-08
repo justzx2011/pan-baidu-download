6170f5529bbc0966de7a7f21ee6df8719eec898f
banbanchs <memory.silentvoyage@gmail.com>
2014-03-08 12:59:02 +0800

    Support album files and files download

--- command/home.py
+++ command/home.py
@@ -30,7 +30,7 @@ class FileList(object):
         # &bdstoken=%(token)s % {'token': token}
         self._url = 'http://pan.baidu.com/pcloud/feed/getsharelist?t=%(unix_time)d&category=0&auth_type=1' \
                     '&request_location=share_home&start=%(index)d&limit=%(limit)d&query_uk=%(uk)s' \
-                    '&channel=chunlei&clienttype=0&web=1' % {'unix_time': int(time.time()), 'index': (self.page-1),
+                    '&channel=chunlei&clienttype=0&web=1' % {'unix_time': int(time.time()), 'index': (self.page - 1),
                                                              'limit': self.limit, 'uk': self.uk}
         data = json.load(self.opener.open(self._url))
         if data.get('errno', -1):
@@ -39,25 +39,23 @@ class FileList(object):
         if not total_count:
             raise IndexError('该用户分享为空')
         self._raw_list = data.get('records', [])
-        _filelist = map(filter_dict, self._raw_list)
-        self.filelist = filter(bool, _filelist)
 
     def __len__(self):
-        return len(self.filelist)
+        return len(self._raw_list)
 
     def show(self):
         for index, item in enumerate(self._raw_list):
             # if is dir or set
             if item.get('filecount') > 1 or item.get('dir_cnt'):
-                print "%s%2d %s%s" % (bcolor.OKBLUE, index+1, item.get('title'), bcolor.ENDC)
+                print "%s%2d %s%s" % (bcolor.OKBLUE, index + 1, item.get('title'), bcolor.ENDC)
             else:
-                print "%s%d %s%s" % (bcolor.OKGREEN, index+1, item.get('title'), bcolor.ENDC)
+                print "%s%2d %s%s" % (bcolor.OKGREEN, index + 1, item.get('title'), bcolor.ENDC)
 
     def next(self):
         self.page += 1
         self._url = 'http://pan.baidu.com/pcloud/feed/getsharelist?t=%(unix_time)d&category=0&auth_type=1' \
                     '&request_location=share_home&start=%(index)d&limit=%(limit)d&query_uk=%(uk)s' \
-                    '&channel=chunlei&clienttype=0&web=1' % {'unix_time': int(time.time()), 'index': (self.page-1),
+                    '&channel=chunlei&clienttype=0&web=1' % {'unix_time': int(time.time()), 'index': (self.page - 1),
                                                              'limit': self.limit, 'uk': self.uk}
         data = json.load(self.opener.open(self._url))
         if data.get('errno', -1):
@@ -65,8 +63,7 @@ class FileList(object):
         total_count = data.get('total_count', 0)
         if not total_count:
             raise IndexError('该用户分享为空')
-        self._raw_list = iter(data.get('records', []))
-        self.filelist = map(filter_dict, self._raw_list)
+        self._raw_list = data.get('records', [])
 
     def download(self):
         # Bug: when input code is directory, may raise a error
@@ -81,19 +78,40 @@ class FileList(object):
                 continue
             elif 'a' in seq:
                 # download all
-                seq = range(1, self.limit+1)
+                seq = range(1, self.limit + 1)
             else:
                 try:
                     # filter the input greater than limit
-                    seq = [int(i) for i in seq if self.limit >= len(self.filelist) >= int(i)]
+                    seq = [int(i) for i in seq if self.limit >= len(self) >= int(i)]
                 except ValueError:
                     raise ValueError("输入错误！")
+
+            # parser json and download it
             for i in seq:
-                info = self.filelist[i-1]
-                pan = BaiduDown(raw_link='', filename=info.get('server_filename', ''), bdstoken=self.bdstoken,
-                                fs_id=info.get('fs_id'), uk=self.uk, shareid=info.get('shareid'),
-                                timestamp=info.get('time_stamp'), sign=info.get('sign'))
-                download_command(pan.filename, pan.link)
+                info = filter_dict(self._raw_list[i - 1])
+                print info
+                # if share type is album
+                if info.has_key('operation'):
+                    for t in info.get('operation'):
+                        filelist = t.get('filelist')
+                        if not filelist:
+                            continue
+                        download_seq = [value for f in filelist for (key, value) in f.items()
+                                        if key in ['server_filename', 'dlink']]
+                        for j in range(len(download_seq) / 2):
+                            dlink = download_seq.pop()
+                            server_filename = download_seq.pop()
+                            download_command(server_filename, dlink)
+                # if share type is not a dir
+                elif info.has_key('filelist'):
+                    shareid = info.get('shareid')
+                    for fl in info.get('filelist'):
+                        pan = BaiduDown(filename=fl.get('server_filename'), bdstoken=self.bdstoken,
+                                        fs_id=fl.get('fs_id'),
+                                        uk=self.uk, shareid=shareid, timestamp=fl.get('time_stamp'),
+                                        sign=fl.get('sign'))
+                        download_command(pan.filename, pan.link)
+                # TODO: support dir download
             break
 
 
